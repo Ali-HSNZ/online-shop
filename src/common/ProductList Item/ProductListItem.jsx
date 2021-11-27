089b526f4e428dc76962bc78d996476c5b41046e
Ali Hassanzadeh <Aliatraby@gmail.com>
2021-11-27 13:09:00 +0330

    change Design ProductList Item

--- src/common/ProductList Item/ProductListItem.jsx
+++ src/common/ProductList Item/ProductListItem.jsx
@@ -1,12 +1,13 @@
 import Styles from './ProductListItem.module.css'
 import { BiHeart} from "react-icons/bi";
 import {UseCart, UseCartDispatch } from '../../Context/cartContext/CartProvider'
-import { BiTrashAlt } from "react-icons/bi";
+import { BiTrashAlt , BiShoppingBag } from "react-icons/bi";
 import { Link } from "react-router-dom";
 
 const ProductListItem = ({item}) => {
     const dispatch = UseCartDispatch()
     const {cart} = UseCart()
+
     const checkProductInCart=  (state , product)=>{
         const item = state.findIndex(item => item.id === product.id)
         if(item < 0) {return false}else{return true}
@@ -22,42 +23,51 @@ const ProductListItem = ({item}) => {
     }
 
     return (  
-        <div className={Styles.item} key={item.id} dir="ltr">
-            <div className={Styles.imgParent}>
-                <img src={item.image} alt={item.title}/>
-                <div className={Styles.like_bookMark_parent}>
-                    <div className={Styles.likeParent}>
-                        <BiHeart size="1.3em" style={{color:'red'}}/>
-                            <p>{item.rating.rate}</p>
-                            <span>({item.rating.count})</span>
+        <div className={Styles.itemParent}>
+            <div className={Styles.itemParent_center}>
+
+                <div className={Styles.item}>
+                    {item.offPrice && <div className={Styles.header_offPrice}>{item.offPrice}%</div>}
+
+                    <div className={Styles.item_header}>
+                        <div className={Styles.likeParent}>
+                            <BiHeart size="1.3em" style={{color:'red'}}/>
+                                <p>{item.rating.rate}</p>
+                                <span>({item.rating.count})</span>
+                        </div>
                     </div>
-                </div>
-            </div>
-            <div className={Styles.titleParent}>
-                <p className={Styles.title} title={item.title}>{item.title.length >= 20 ? item.title.substring(0,20)+'...' : item.title}</p>
-            </div>
-            <div className={Styles.footer}>
-                <div className={Styles.addToCartParent}>
-                        {checkProductInCart(cart , item) ?   
-                            <> 
-                                <Link to="/cart">سبد خرید</Link>
 
-                                <section className={Styles.trashBtn} onClick={() => deleteProduct(item)}>
-                                    حذف
-                                    <BiTrashAlt style={{cursor:'pointer'}}  size="1em"/> 
+                    <div className={Styles.imgParent}>
+                        <img src={item.image} alt={item.title}/>
+                    </div>
 
-                                </section>
-                            </> : 
-                            <button className={Styles.addTodoBtn} onClick={()=>addToCartHandler(item)}>
-                                خرید محصول
-                            </button>
-                        }
-                </div>
-                <div className={Styles.price}>
-                    <p>${item.price}</p> 
+                    <div className={Styles.titleParent}>
+                        <p className={Styles.title} title={item.title}>{item.title.length >= 20 ? item.title.substring(0,20)+'...' : item.title}</p>
+                    </div>
+
+                    <div className={Styles.item_footer} dir="rtl">
+                        <p dir="rtl">قیمت محصول : {item.price}$</p>
+                        {item.discount && <p className={Styles.itemOffPrice} dir="rtl">تخفیف : {item.discount}$</p>}
+                    </div>
+
+
+                <div className={Styles.itemActionParent}>
+                    {checkProductInCart(cart , item) === true ? (
+                        <Link to="/cart" className={Styles.itemAcrionActive}>سبد خرید</Link>
+                    ) : (
+                        <button className={Styles.itemAcrion} onClick={()=> addToCartHandler(item)}>
+                            <p>خرید محصول</p>
+                            <BiShoppingBag size="1.5em"/>
+                        </button>
+                    )
+                    }
+
+                    
                 </div>
             </div>
         </div>
+    </div>
+      
     );
 }
  
