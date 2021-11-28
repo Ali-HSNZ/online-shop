91cf67ede8ae857d8b34878387492fab83f20a06
Ali Hassanzadeh <Aliatraby@gmail.com>
2021-11-29 02:33:35 +0330

    #1 Create NewLogin Component

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,46 +1,86 @@
 import Styles from './Header.module.css'
 import Logo from '../../image/logo.png'
-import {BiShoppingBag , BiUser} from "react-icons/bi";
-import { FaUserCheck } from "react-icons/fa";
-import { NavLink } from 'react-router-dom';
+import {BiShoppingBag , BiUser , BiSearch , BiHeart} from "react-icons/bi";
+import  LoginStyles from'./LoginStyles.module.css'
+import { Link, NavLink } from 'react-router-dom';
 import { UseCart } from '../../Context/cartContext/CartProvider';
 import { User } from '../../Context/userProvider/UserProvider';
-const Header = ( ) => {
+import { withRouter } from 'react-router';
+import { useState } from 'react';
+import { FiAlertTriangle } from "react-icons/fi";
+import NewUserLogin from '../newUserLogin/NewUserLogin';
+
+
+const Header = (props ) => {
+
+
     const user = User()
     const {cart} = UseCart()
+    const [isUserLogin , setIsUserLogin] = useState(false)
+
+
+    const UserLogin = ()=>{
+        return(
+            <div>
+
+                <div className={LoginStyles.parent} onClick={(e)=>setIsUserLogin(false)}>
+                </div>
+
+                <div className={LoginStyles.main} onClick={()=>setIsUserLogin(true)}>
+                    <NewUserLogin/>
+                </div>
+                
+            </div>
+        )
+    }
+
+
     return (
+        <>
         <div className={Styles.parent}>
 
             <div className={Styles.header}>
 
                 <div className={Styles.header_left}>
 
-                    <NavLink activeClassName={Styles.activeLink} to="/cart"  className={Styles.iconParent}>
+                    <NavLink activeClassName={Styles.activeLink} to="/cart"  className={Styles.iconParent}  onClick={(e)=>setIsUserLogin(false)}>
                         <BiShoppingBag className={Styles.iconStyle} size="1.7em"/>
                         {cart.length > 0 && <p className={Styles.cartCount}> {cart.length}</p>}
                     </NavLink>
 
-                    <NavLink activeClassName={Styles.activeLink} className={Styles.iconParent} to={`${user ? "/user-profile" : "/user-login?redirect=Home"}`}>
-                        {user ? <FaUserCheck className={Styles.iconStyle} size="1.7em"/> :  <BiUser  className={Styles.iconStyle} size="1.7em"/>}              
+
+                    <NavLink activeClassName={Styles.activeLink} className={Styles.iconParent} to={`/user-like`} exact  onClick={(e)=>setIsUserLogin(false)}>
+                       <BiHeart className={Styles.iconStyle} size="1.7em"/>            
                     </NavLink>
-                   
+
+                    {/* <button className={Styles.iconParent} to={`${user ? "/user-profile" : "/user-login?redirect=Home"}`}> */}
+                    <button className={Styles.iconParent_Button} onClick={()=> setIsUserLogin(true)}>
+                        {/* {user ? <FaUserCheck className={Styles.iconStyle} size="1.7em"/> :  <BiUser  className={Styles.iconStyle} size="1.7em"/>}               */}
+                        <BiUser  className={Styles.iconStyle} size="2em"/>            
+                       
+                    </button>
+
+
+                    <button className={Styles.iconParent_Button} >
+                       <BiSearch className={Styles.iconStyle} size="2em"/>            
+                    </button>
+
                 </div>
 
                 
                 <div className={Styles.header_right}>
                     <NavLink activeClassName={Styles.activeLink} to="/" exact>خانه</NavLink>
-                    <div className={Styles.logoParent}><img className={Styles.logoParent_Img} alt="لوگو" src={Logo}/></div>
+                    <div className={Styles.logoParent}>
+                        <Link to="/" className={Styles.link_Logo}>
+                            <img className={Styles.logoParent_Img} alt="لوگو" src={Logo}/>
+                        </Link>
+                    </div>
                 </div>
-
-               
-
-            
-
             </div> 
-          
         </div>
+          {isUserLogin &&   <UserLogin/>}
+          </>
     );
   };
-  
-  export default Header;
-  
+
+  export default withRouter(Header);
---
+++ src/Components/newUserLogin/NewUserLogin.jsx
@@ -0,0 +1,108 @@
+import { AiFillCaretUp } from "react-icons/ai";
+import  LoginStyles from '../Header/LoginStyles.module.css'
+import { useFormik } from 'formik'
+import * as Yup from 'yup';
+import { FiAlertTriangle } from "react-icons/fi";
+import { useEffect, useState } from 'react';
+import { userLogin } from '../../services/loginService';
+import { toast } from 'react-toastify';
+import { Link } from 'react-router-dom';
+import { UserDispatch , User } from '../../Context/userProvider/UserProvider';
+import {useQuery} from '../../hooks/useQuery'
+import SmallLoading from '../../common/small Loding/SmallLoading'
+
+
+const NewUserLogin = (props) => {
+
+    const dispatchUser = UserDispatch()
+
+    const [isLoading , setIsLoading] = useState(false)
+
+
+    const initialValues = {
+        email : '',
+        password : '',
+    }
+
+    const passwordRegExp = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8})/
+
+    const validationSchema = Yup.object({
+        email: Yup.string().email("ایمیل را به درستی وارد کنید").required("ایمیل را وارد کنید"),
+        password : Yup.string().required("رمز عبور خود را وارد کنید").matches(passwordRegExp , "رمز بیشتر از 8 کاراکتر باشد ( انگلیسی : حرف کوچک، بزرگ و عدد)"),
+    })
+
+
+
+
+
+    const onSubmit = async(values) => {
+        const {email , password} = values
+
+        const userData = {email , password}
+        setIsLoading(true)
+       
+            try {
+                const data = await userLogin(userData)     
+                setIsLoading(false)
+                toast.success("با موفقیت وارد شدید")
+                dispatchUser(JSON.parse( data.config.data))
+            } catch (e) {
+                toast.error(e.response.data.message)
+                setIsLoading(false)
+            }
+        
+      
+
+    }
+
+
+
+
+
+
+    const formik =useFormik({
+        initialValues,
+        onSubmit,
+        validationSchema,
+        validateOnMount : true
+    })
+
+    return (  
+        <form  onSubmit={formik.handleSubmit}>
+                            <div className={LoginStyles.arrow}>
+                        <AiFillCaretUp size="2em"/>
+                    </div>
+
+                    <p className={LoginStyles.title}>ورود به سایت</p>
+
+                    <div className={LoginStyles.group}>
+                        <p dir="rtl">ایمیل : </p>
+                        <input type="text" onChange={formik.handleChange} name='email' placeholder="ایمیل خود را وارد کنید..." onBlur={formik.handleBlur} dir="rtl"/>
+                        {formik.errors.email && formik.touched.email && <p className={LoginStyles.errorText} style={{fontSize:'13px'}}>{formik.errors.email}</p>}
+
+                    </div>
+
+                    <div className={LoginStyles.group}>
+                        <p dir="rtl">رمز عبور : </p>
+                        <input type="text" onChange={formik.handleChange} name='password' placeholder="رمز عبور خود را وارد کنید..." onBlur={formik.handleBlur} dir="rtl"/>
+                        {formik.errors.password && formik.touched.password && <p className={LoginStyles.errorText} style={{fontSize:'13px'}}>{formik.errors.password}</p>}
+
+                    </div>
+                    
+
+                    <div>
+                    <button 
+                        type="submit" 
+                        // disabled={!formik.isValid} title={!formik.isValid ? "لطفا مقادیر خواسته شده را وارد کنید" : ""}
+                        className={`${LoginStyles.submitBtn} ${LoginStyles.submitBtn_active}`}>
+                         ورود
+        
+                        
+                     {/* <FiAlertTriangle size="1.3rem" style={{marginLeft:"8px" , color:'#ff6969'}}/> */}
+                    </button>
+                    </div>
+            </form>
+    );
+}
+ 
+export default NewUserLogin;
