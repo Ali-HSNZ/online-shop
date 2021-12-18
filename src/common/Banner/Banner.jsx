43df82d634b464fe3d447eef745aa29835d19d86
Ali Hassanzadeh <Aliatraby@gmail.com>
2021-12-18 19:16:50 +0330

    Replace Functional Component To Class Component => Banner Component

--- src/common/Banner/Banner.jsx
+++ src/common/Banner/Banner.jsx
@@ -2,17 +2,24 @@ import Styles from './Banner.module.css'
 import { Link } from 'react-router-dom';
 import bannerImage from '../../image/33.jpg'
 import womanClouting from '../../image/womanClouting.jpg'
+import React from 'react';
 
 
-const Banner = ({category}) => {
-    const categoryImage = category === "jewelery" ? bannerImage : womanClouting
-    return (  
-        <div className={Styles.parent}>
-            <Link to={`/category?name=${category}`}>
-                <img src={categoryImage} alt="product img"/>
-            </Link>
-        </div>
-    );
+// const Banner = ({category}) => {
+class Banner extends React.Component{
+    render(){
+        const category = this.props.category;
+
+        const categoryImage = category === "jewelery" ? bannerImage : womanClouting;
+        return (  
+            <div className={Styles.parent}>
+                <Link to={`/category?name=${category}`}>
+                    <img src={categoryImage} alt="product img"/>
+                </Link>
+            </div>
+        );
+    }
+
 }
  
 export default Banner;
