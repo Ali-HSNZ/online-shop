e6bb48656abe4bdd15da2090255ba864fdaeeeee
Ali Hassanzadeh <Aliatraby@gmail.com>
2021-12-18 14:46:06 +0330

    Replace => Functional Component To Class Component ==> Footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,10 +1,14 @@
+import React from 'react';
 import Styles from './Footer.module.css'
-const Footer = () => {
+
+class Footer extends React.Component{
+  render(){
     return (  
         <div className={Styles.parent}>
             <p>Made with ♥ by Ali Hassanzadeh</p>
         </div>
     );
+  }
 }
  
 export default Footer;
