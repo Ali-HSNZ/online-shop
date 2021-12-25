aec2bcb519c1283d505508881a3c42134818dd7f
Ali Hassanzadeh <Aliatraby@gmail.com>
2021-12-25 15:30:02 +0330

    improve the code => Favorite Products Page

--- src/Pages/FavoriteProducts/FavoriteProducts.jsx
+++ src/Pages/FavoriteProducts/FavoriteProducts.jsx
@@ -10,15 +10,19 @@ const FavoriteProducts = () => {
 
     return ( 
         <>
-            {filterLike.length > 0 && <div className={Styles.parent}>
-                {filterLike.length > 0 && filterLike.map((product , index) => {
-                    return(
-                        <div key={index}>
-                            <ProductListItem isLink={true} item={product}/>
-                        </div>
-                    )
-                })}
-            </div>}
+            {
+                filterLike.length > 0 && (
+                    <div className={Styles.parent}>
+                        {filterLike.map((product , index) => {
+                            return(
+                                <div key={index}>
+                                    <ProductListItem isLink={true} item={product}/>
+                                </div>
+                            )
+                        })}
+                    </div>
+                )
+            }
             {filterLike.length === 0 && (
                 <div className={Styles.alert_product}>
                 <p>میرم</p>
